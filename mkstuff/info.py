c9f2c0c069cfeff11d164230d7c6c2060df7e74d
Martin Kilbinger <martin.kilbinger@cea.fr>
2019-09-19 18:02:48 +0200

    Made changes according to Sam's template

--- mkstuff/info.py
+++ mkstuff/info.py
@@ -2,24 +2,32 @@
   
 """INFO
 
-Set the package information
+Set the package information for `mkstuff'
 
 :Author: Martin Kilbinger, <martin.kilbinger@cea.fr>
 
 :Date: 18/04/2019
 """
 
-__version__ = '1.2'
-__whoami__  = 'mkstuff'
-__author__  = 'Martin Kilbinger'
-__email__   = 'martin.kilbinger@cea.fr'
+# Release version
+version_info = (1, 2, 0)
+__version__ = '.'.join(str(c) for c in version_info)
 
-# Python dependencies
-__python__depend__ = ['numpy', 'subprocess', 'scipy', 'textwrap', 'astropy']
+# Package details
+__author__ = 'Martin Kilbinger'
+__email__ = 'martin.kilbinger@cea.fr'
+__year__ = 2019
+__url__ = 'https://github.com/martinkilbinger/mkstuff' 
+__description__ = 'Useful but random scientific python modules and scripts'
 
-# Can we add optional packages? colorama
+# Dependencies
+__requires__ = ['numpy', 'subprocess', 'scipy', 'textwrap', 'astropy']
 
-# System dependencies
-
-__system_depend__ = []
+# Default package properties
+__license__ = 'MIT'
+__about__ = ('{} \n\n Author: {} \n Email: {} \n Year: {} \n {} \n\n'
+             ''.format(__name__, __author__, __email__, __year__,
+                       __description__))
+__setup_requires__ = ['pytest-runner', ]
+__tests_require__ = ['pytest', 'pytest-cov', 'pytest-pep8']
 
